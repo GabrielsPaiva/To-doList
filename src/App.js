d881f2a08111b231a7d1e23722ed4d5e23af8cba
Gabriel Souza <gabrielpaiva423@gmail.com>
2022-03-14 17:03:07 -0300

    adjusting styles

--- src/App.js
+++ src/App.js
@@ -40,7 +40,6 @@ ${media.lessThan('medium')`
 width: 100%;
 border-radius: 0px;
 `}
-${customMedia.lessThan('')}
 `
 const Title = styled.h2`
 padding-bottom: 10%;
@@ -54,6 +53,12 @@ border-radius: 25px;
 width: 90%;
 height: 10%;
 margin-bottom: 5%;
+
+${customMedia.lessThan('mobileS')`
+background-color: transparent;
+flex-direction: column;
+margin-top: 2%;
+`}
 `
 const Input = styled.input`
 border: none;
@@ -71,6 +76,7 @@ font-size: 50px;
 width: 10%;
 height: 50%;
 margin-left: 4%;
+outline: none;
 
 ${media.lessThan('medium')`
 height: 10vw;
@@ -103,11 +109,11 @@ font-size: 25px;
 list-style: none;
 width: 90%;
 height: fit-content;
+word-break: break-word;
 `
 const Erase = styled.img`
 border-radius: 50%;
-width: 10%;
-height: 90%;
+width: 7vw;
 `
 const Clear = styled.button`
 display: flex;
